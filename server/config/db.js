92f7009a11fba4f30396a5dfc0c6e12778795e2e
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-11-11 11:53:54 +0100

    more options on db.js for db connection.

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,16 @@ const { MONGO_URI, PORT } = require('../../consts');
 
 async function connectDB() {
   try {
-    const db = await mongoose.connect(MONGO_URI);
+    const db = await mongoose.connect(MONGO_URI, {
+      // Options de connections à la BDD > Meilleure compatibilité et performances.
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log(`Connected to DB: ${db.connection.name} on ${PORT}`);
+    console.log(`Connected to DB: ${db.connection.name} on port ${PORT}`);
   } catch (error) {
-    console.log('Could not connect to DB: ', error);
+    console.error('Could not connect to DB: ', error);
+    process.exit(1); // Arrête le processus si la connexion échoue
   }
 }
 
