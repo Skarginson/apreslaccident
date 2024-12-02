9bd6a2256a9ed8236cf73f9dbd9dec55d1bfa47e
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-12-02 10:38:25 +0100

    game.router done and protection.middleware wip

---
+++ server/middleware/protection.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const protectionMiddleware = (req, res, next) => {
+  // Récupérer le token depuis les en-têtes de la requête
+  const token = req.header('Authorization')?.split(' ')[1]; // Bearer <token>
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: 'Access denied. No token provided.' });
+  }
+
+  try {
+    // Vérifier et décoder le token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Ajouter les données de l'utilisateur décodées à l'objet req
+    next(); // Passer au middleware ou à la route suivante
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid token.' });
+  }
+};
+
+module.exports = protectionMiddleware;
---
+++ server/routes/game.routers.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const Game = require('../models/Game');
+const { handleNotFound } = require('../../utils');
+const protectionMiddleware = require('../middlewares/protection.middleware');
+
+// Protéger les routes suivantes avec le middleware (si nécessaire)
+router.use(protectionMiddleware);
+
+router.get('/', async (_, res, next) => {
+  try {
+    const games = await Game.find();
+    res.json(games);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const game = await Game.findById(id);
+    if (!game) {
+      handleNotFound(res);
+      return;
+    }
+    res.json(game);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Créer une nouvelle partie
+router.post('/', async (req, res, next) => {
+  const { userId } = req.body;
+
+  try {
+    const newGame = await Game.create({ userId });
+    res.status(201).json(newGame);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const updatedGame = await Game.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedGame) {
+      handleNotFound(res);
+      return;
+    }
+
+    res.json(updatedGame);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    await Game.findByIdAndDelete(id);
+    res.json({ message: 'Game deleted successfully' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
